371ec8f37d8a5a15d7446cc043a4a6d65e8ecd43
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-12-29 14:41:42 +0100

    I've created the modal component

---
+++ src/components/ModalView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>Modal</section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,5 +34,14 @@ watchEffect(async () => {
 </script>
 
 <template>
-  <section class="mt-12">Welcome</section>
+  <section class="mt-12 flex justify-center items-center">
+    <p v-if="loading">Carregando...</p>
+    <p v-else-if="error">Erro de internet por favor tente mais tarde!</p>
+    <div class="w-[75%] flex justify-center items-center flex-wrap gap-5" v-else>
+      <div v-for="{ fotos, id, nome } in data" :key="id" class="w-40 flex-auto">
+        <h1>{{ nome }}</h1>
+        <img :src="fotos[0].src" :alt="fotos[0].titulo" />
+      </div>
+    </div>
+  </section>
 </template>

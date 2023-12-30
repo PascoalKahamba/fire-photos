efa4717aeddafe79c3cbe0f15ef02ddacf02e141
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-12-30 14:58:24 +0100

    I'm building the modal component

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -8,13 +8,47 @@ const uniquePhoto = computed(() => store.data.filter((data) => data.id === store
 
 <template>
   <section
-    class="fixed z-[1000] w-full h-full bg-slate-800 flex justify-center items-center top-0 opacity-5"
-    data-aos="fade-down"
+    class="fixed z-[1000] w-full h-full bg-slate-200 flex justify-center items-center top-0 opacity-5"
+    data-aos="zoom-in"
     data-aos-duration="200"
   >
-    <div v-for="photo in uniquePhoto" :key="photo.id" class="w-[50%] p-5 rounded-lg">
-      <img :src="photo.fotos[0].src" :alt="photo.fotos[0].src" class="w-full" />
-      <h1>{{ photo.nome }}</h1>
+    <div
+      v-for="photo in uniquePhoto"
+      :key="photo.id"
+      class="w-[72%] overflow-y-auto rounded-lg bg-white flex gap-2 flex-col mb-40"
+      @click="store.modal = false"
+    >
+      <div class="w-full h-[380px]">
+        <img :src="photo.fotos[0].src" :alt="photo.fotos[0].src" class="w-full h-full" />
+      </div>
+      <div class="p-2">
+        <h1 class="text-center font-normal text-xl">{{ photo.nome }}</h1>
+        <div class="flex-1">
+          <p>Foto Bunita</p>
+          <p>Foto Bunita</p>
+          <p>Foto Bunita</p>
+        </div>
+        <form class="w-full flex gap-3">
+          <input
+            type="text"
+            id="comments"
+            placeholder="Digite um comentario"
+            class="border-none p-3 w-[70%] bg-slate-200 rounded-md outline-blue-700"
+          />
+          <button
+            type="submit"
+            class="bg-blue-700 p-3 self-center w-28 radius rounded-lg text-slate-200 hover:bg-blue-500 transition-all"
+          >
+            Comentar
+          </button>
+        </form>
+        <button
+          class="bg-blue-700 p-3 w-28 radius rounded-lg text-slate-200 hover:bg-blue-500 transition-all self-center"
+          @click="store.modal = false"
+        >
+          Fechar Modal
+        </button>
+      </div>
     </div>
   </section>
 </template>

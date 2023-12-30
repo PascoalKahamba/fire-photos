22a108237c90050f1fefd428c8a68038d3d7a9d3
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-12-30 15:44:34 +0100

    I've created the postCommint function

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useCounterStore } from '../stores/counter';
 
 const store = useCounterStore();
+const comment = ref('');
+const error = ref(false);
+const errorComment = computed(() => errorMessage(comment.value));
 const uniquePhoto = computed(() => store.data.filter((data) => data.id === store.specialId));
+
+function errorMessage(field: string) {
+  return !field || (!Number.isNaN(+field) && true);
+}
+
+function postComent() {
+  if (errorMessage(comment.value)) {
+    error.value = true;
+    return;
+  }
+  console.log('New Coment');
+  comment.value = '';
+  error.value = false;
+}
 </script>
 
 <template>
   <section
-    class="fixed z-[1000] w-full h-full bg-slate-200 flex justify-center items-center top-0 opacity-5"
+    class="fixed z-[1000] w-full h-full bg-slate-100 flex justify-center items-center top-0 opacity-10"
     data-aos="zoom-in"
     data-aos-duration="200"
   >
@@ -16,22 +33,22 @@ const uniquePhoto = computed(() => store.data.filter((data) => data.id === store
       v-for="photo in uniquePhoto"
       :key="photo.id"
       class="w-[72%] overflow-y-auto rounded-lg bg-white flex gap-2 flex-col mb-40"
-      @click="store.modal = false"
     >
       <div class="w-full h-[380px]">
         <img :src="photo.fotos[0].src" :alt="photo.fotos[0].src" class="w-full h-full" />
       </div>
-      <div class="p-2">
+      <div class="p-2 flex gap-2 flex-col">
         <h1 class="text-center font-normal text-xl">{{ photo.nome }}</h1>
         <div class="flex-1">
           <p>Foto Bunita</p>
           <p>Foto Bunita</p>
           <p>Foto Bunita</p>
         </div>
-        <form class="w-full flex gap-3">
+        <form class="w-full flex gap-3 items-center" @submit.prevent="postComent">
           <input
             type="text"
             id="comments"
+            v-model="comment"
             placeholder="Digite um comentario"
             class="border-none p-3 w-[70%] bg-slate-200 rounded-md outline-blue-700"
           />
@@ -42,8 +59,11 @@ const uniquePhoto = computed(() => store.data.filter((data) => data.id === store
             Comentar
           </button>
         </form>
+        <span class="text-red-600 italic -mt-2" v-show="error && errorComment"
+          >Senha invalido!</span
+        >
         <button
-          class="bg-blue-700 p-3 w-28 radius rounded-lg text-slate-200 hover:bg-blue-500 transition-all self-center"
+          class="bg-blue-700 p-3 w-32 radius rounded-lg text-slate-200 hover:bg-blue-500 transition-all self-center"
           @click="store.modal = false"
         >
           Fechar Modal
